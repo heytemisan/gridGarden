556066e2786e4e3c844ba727e19d2a67e209de5e
Heytemisan <temyicy@gmail.com>
2022-01-08 18:00:02 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>CSS GRIDS</h1>
+      <div className="layout">
+        <div className="logo">logo</div>
+        <div className="header">header</div>
+        <div className="nav">nav</div>
+        <div className="content">content</div>
+        <div className="footer">footer</div>
+      </div>
     </div>
   );
 }
